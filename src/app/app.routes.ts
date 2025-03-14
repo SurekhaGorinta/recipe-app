c682898b3a8d1c7551d05a3dba1b240a5f3d1b54
SurekhaGorinta <surekhasunny64@gmail.com>
2025-03-13 22:08:27 -0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'recipes', component: RecipesComponent },
   { path: 'add-recipe', component: AddRecipeComponent }
 ];
